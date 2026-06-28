395319cd381e6625eaa245e6a82a2e1cd73dfad8
Anurag kumar <anuragkumar1252003@gmail.com>
2026-06-28 16:27:17 +0530

    learning about bitwise operators

---
+++ bitwise.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a =1002, b=10003;
+    cout<< "a&b: "<< (a&b) << endl;
+    cout<< "a|b: "<< (a|b)<< endl;
+    cout<< "~a: "<< (~a)<< endl;
+    cout<< "a^b: "<< (a^b)<< endl;
+}
