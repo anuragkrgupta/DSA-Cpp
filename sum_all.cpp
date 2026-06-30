2c2feb945267ddb21a10b69f337df31fb2889e11
Anurag kumar <anuragkumar1252003@gmail.com>
2026-06-30 11:46:31 +0530

    sum of all numbers using for loop

---
+++ sum_all.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "sum_all.h"
+using namespace std;
+int main() {
+    int n;
+    cout << "enter the limit: ";
+    cin >> n;
+    cout << "sum of all the number from 1 to "<< n <<": ";
+    long long sum = 0;
+    for (
+        int i=1; 
+        i<=n;
+        i++
+    ) {
+        sum += i;
+    }
+    cout << sum;
+}
