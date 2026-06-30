4b12af72decd6a0d4a0360f273d3fd7d2ce32a0c
Anurag kumar <anuragkumar1252003@gmail.com>
2026-06-30 11:45:19 +0530

    for loop explained

---
+++ for_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter the limit: ";
+    cin >> n;
+    cout << "printing from 1 to " << n << endl;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << endl;
+    }
+}
