10958d98e3a420943f5ed69612510dffa18f9089
Anurag kumar <anuragkumar1252003@gmail.com>
2026-07-09 02:09:28 +0530

    refactor prime number logic to use a function for better readability

--- prime_number.cpp
+++ prime_number.cpp
@@ -1,21 +1,47 @@
+// #include<iostream>
+// using namespace std;
+// int main () {
+//     int isPrime = 1;
+//     int n;
+//     cin >> n;
+
+//     for(int i=2; i<n; i++){
+//         if(n%i == 0){
+//             isPrime = 0;
+//             break;
+
+//         }
+//     }
+//     if(isPrime == 0){
+//         cout << n <<" is not a prime number.";
+//     }
+//     else{
+//         cout<< n << " is a prime number";
+//     }
+// }
+
+//lets try it by using function
 #include<iostream>
 using namespace std;
-int main () {
-    int isPrime = 1;
-    int n;
-    cin >> n;
-
+bool isPrime(int n){
     for(int i=2; i<n; i++){
-        if(n%i == 0){
-            isPrime = 0;
-            break;
-
+        if(n%i==0){
+            return 0;
         }
     }
-    if(isPrime == 0){
-        cout << n <<" is not a prime number.";
+    return 1;
+    //return isPrime;
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+    if (isPrime(n)){
+        cout<< "is a prime number";
+
     }
     else{
-        cout<< n << " is a prime number";
+     cout<< " not a prime number";
     }
 }
