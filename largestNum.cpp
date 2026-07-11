9bb6a9b9df82c146152b360b0551a2f0afdf96fb
Anurag kumar <anuragkumar1252003@gmail.com>
2026-07-12 00:32:34 +0530

    Refactor largestNum.cpp to improve clarity and structure of the largest number finding function

--- largestNum.cpp
+++ largestNum.cpp
@@ -1,10 +1,28 @@
-#include<iostream>
+// finding the largest number without using array, approach is - storing the input in a variable and comparing it continuously with input , if the input is smaller than the previous number take another input otherwise store the new input in a variable.
+
+#include <iostream>
 using namespace std;
-int largestNum(){
-    int n;
-    cin >> n;
-    int compare = n;
-    while (n != 0){
-        
+int largestNum()
+{
+    int input;
+    cin >> input;
+    int currentNum = input;
+    cout << "enter the next integer: ";
+    cin >> input;
+    while (input != 0)
+    {
+        if (currentNum <= input)
+        {
+            currentNum = input;
+        }
+        cout << "enter the next integer: ";
+        cin >> input;
     }
+
+    cout << currentNum << " : is the largest number";
+    return currentNum;
+}
+int main()
+{
+    largestNum();
 }
