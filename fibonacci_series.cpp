df4ce89017f5c651868ce750c6fe05b8c3f7c645
Anurag kumar <anuragkumar1252003@gmail.com>
2026-07-11 18:53:31 +0530

    function-based approach

--- fibonacci_series.cpp
+++ fibonacci_series.cpp
@@ -1,21 +1,38 @@
-#include<iostream>
-using namespace std;
-int main() {
-    int n;
-    cin >> n;
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cin >> n;
 
-    int firstNumber = 0;
-    int nextNumber = 1;
-    cout << firstNumber << " " << nextNumber << " " ;
-    int sum=0;
-    for (int i=0; i<=n; i++){
-        sum= firstNumber + nextNumber;
-        cout<< sum << " ";
-        firstNumber = nextNumber;
-        nextNumber = sum;
-    }
+//     int firstNumber = 0;
+//     int nextNumber = 1;
+//     cout << firstNumber << " " << nextNumber << " " ;
+//     int sum=0;
+//     for (int i=3; i<=n; i++){
+//         sum= firstNumber + nextNumber;
+//         cout<< sum << " ";
+//         firstNumber = nextNumber;
+//         nextNumber = sum;
+//     }
+// }
 
-    
+//fibonacci using functions
 
-
-}
+// #include<iostream>
+// using namespace std;
+// void fibo(){
+//     int n;
+//     cin >> n;
+//     int firstNumber = 0;
+//     int secondNumber = 1;
+//     cout << firstNumber << " " << secondNumber << " ";
+//     for (int i = 3; i<=n; i++){
+//         int nextNumber = firstNumber + secondNumber;
+//         cout << nextNumber << " ";
+//         firstNumber = secondNumber;
+//         secondNumber = nextNumber;
+//     }
+// }
+// int main(){
+//     fibo();
+// }
