862cf365a01d65dda05884c52fd7141a49216235
Anurag kumar <anuragkumar1252003@gmail.com>
2026-08-02 01:22:25 +0530

    book allocation problem

---
+++ example7Solved.cpp
@@ -0,0 +1,16 @@
+//book allocation problem
+#include<iostream>
+using namespace std;
+
+int func(int arr[], int size, int stdnt){
+
+}
+
+int main(){
+int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120};
+int size = sizeof(arr)/sizeof(arr[0]);
+int kid;
+cout<< "enter thee total number of students: ";
+cin >> kid;
+cout << func(arr,size, kid);
+}
