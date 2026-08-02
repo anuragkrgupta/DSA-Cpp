b5359840c5b61785b1c9766173a22432703b97d0
Anurag kumar <anuragkumar1252003@gmail.com>
2026-08-02 20:16:21 +0530

    Paintr's Partition Problem

---
+++ example8Solved.cpp
@@ -0,0 +1,13 @@
+//Paintr's Partition Problem.
+// givven an array/list of length 'N', where the array/list represent the boards and each element of the given array/list represents the length of each board. Some 'K' number of painters are available to paint these boards. Consider that each unit of a board takes 1 unit of time to paint.
+//You are supposed to return the area of the maximum time to get job done of painting all the 'N' boards under a contraints that any painter will only paint the continuos  section of boards.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int painters;
+    cout << "enter the total numbers of painters: ";
+    cin >> painters;
+    
+}
