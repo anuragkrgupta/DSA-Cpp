4a728508714c8f4747a9d328f02ed6a7df4f84bb
Anurag kumar <anuragkumar1252003@gmail.com>
2026-08-03 00:02:19 +0530

    Paintr's Partition Problem

--- example8Solved.cpp
+++ example8Solved.cpp
@@ -4,10 +4,51 @@
 
 #include<iostream>
 using namespace std;
+bool isPossible(int arr[], int size, int painters, int mid){
+    int pCount = 1;
+    int sum = 0;
+    // for(int i = 0; i<size;i++){
+    //     sum += arr[i];
+    // }
+    for(int i = 0; i < size; i++){
+        if(sum + arr[i] <= mid){
+            sum +=arr[i];
+        }
+        else{
+            pCount ++;
+            if(pCount > painters || arr[i] > mid){
+                return false;
+            }
+            sum = arr[i];
+        }
+    }
+    return true;
+}
+int func(int arr[], int size, int painters){
+    int start = 0;
+    int end = 0;
+    for(int i=0; i<size;i++){
+        end +=arr[i];
+    }
+    int ans = -1;
+    while ( start <= end){
+        int mid = start + (end - start)/2;
+        if(isPossible(arr, size, painters, mid)){
+            ans = mid;
+            end = mid - 1;
+        }
+        else{
+            start = mid + 1;
+        }
+    }
+    return ans;
+}
+
 int main(){
     int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(arr)/sizeof(arr[0]);
     int painters;
     cout << "enter the total numbers of painters: ";
     cin >> painters;
-    
+    cout << func(arr,size,painters);
 }
