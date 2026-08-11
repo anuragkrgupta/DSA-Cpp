03e4eb852e9509297d2f4db6978fb4515050b735
Anurag kumar <anuragkumar1252003@gmail.com>
2026-08-12 01:37:28 +0530

    performing selection sort

---
+++ selection_sort.cpp
@@ -0,0 +1,21 @@
+//performing selection sort;
+#include<iostream>
+ int main(){
+    int arr[] = {2,6,3,9,10,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for ( int i=0; i<n-1; i++){
+        int minimum = i;
+
+        for(int j = i+1; j<n; j++){
+            if(arr[j]<arr[minimum]){
+                minimum = j;
+            }
+        }
+        std::swap(arr[minimum], arr[i]);
+    }
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+ } 
