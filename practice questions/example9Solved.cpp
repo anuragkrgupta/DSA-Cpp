8b4d611d3d366e647dc6540a1e69f424dd442c14
Anurag kumar <anuragkumar1252003@gmail.com>
2026-08-11 12:55:09 +0530

    Aggressive Cow Problem

---
+++ practice questions/example9Solved.cpp
@@ -0,0 +1,13 @@
+//Aggressive Cow Problem
+//given array of length 'N', where each element denotes the position of a stall. Now you have 'N' stalls and an integer 'K' which denotes the number of cows that are aggressive. To prevent the cows from hurting each other, you  need to assign the cow to the stall, such that the minimum distance between any two of them is as large as possible. return the largest minimum distance.
+
+#include<iostream>
+int cow(int arr[], int size){
+
+}
+
+int main(){
+    int arr[] = {2,4,6,8};
+    int size = sizeof(arr)/ sizeof(arr[0]);
+    std::cout << cow(arr, size);
+}
