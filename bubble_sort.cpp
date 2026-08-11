1021d56363b1a636764ad666083509bf5d2f4b76
Anurag kumar <anuragkumar1252003@gmail.com>
2026-08-12 02:18:53 +0530

    performing bubble sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -3,11 +3,16 @@
 int sortt(int arr[], int n){
     for(int i = 1; i<n; i++){
         //calculating the rounds we will perform
+        bool swapped = false;
         for(int j = 0; j<n-i; j++){
             if (arr[j] > arr[j + 1])
             {
                 std :: swap(arr[j] , arr[j+1]);
+                swapped = true;
             }
+        }
+        if(swapped == false){
+            break;
         }
     }
 }
