4fbf073ae41fe80b0567ca699121f1f6f37c6109
Anurag kumar <anuragkumar1252003@gmail.com>
2026-08-14 00:05:49 +0530

    easy level questions solved of bubble sort

---
+++ practice questions/example13Solved.cpp
@@ -0,0 +1,55 @@
+// Sort an array using Bubble Sort.
+// [5, 1, 4, 2, 8]
+#include<iostream>
+// void bubble(int arr[], int size){
+//     for(int i = 0; i<size-1; i++){
+//         int minimum = i;
+//         for(int j = i+1; j<size; j++){
+//             if(arr[minimum] > arr[j]){
+//                 minimum = j;
+//             }
+//         }
+//         std::swap(arr[minimum], arr[i]);
+//     }
+// }
+
+// Modify Bubble Sort so that it stops early if the array is already sorted.
+// [1, 2, 3, 4, 5]
+// void bubble(int arr[], int size){
+//     for(int i = 0; i<size-1; i++){
+//         int minimum = i;
+//         for(int j = i+1; j<size; j++){
+//             if(arr[minimum] > arr[j]){
+//                 minimum = j;
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         std::swap(arr[minimum], arr[i]);
+//     }
+// }
+
+// Sort the array in descending order using Bubble Sort.
+// [3, 7, 2, 9, 1]
+void bubble(int arr[], int size){
+    for(int i = 0; i<size-1; i++){
+        int minimum = i;
+        for(int j = i+1; j<size; j++){
+            if(arr[minimum] < arr[j]){
+                minimum = j;
+            }
+        }
+        std::swap(arr[minimum], arr[i]);
+    }
+}
+
+
+int main(){
+    int arr[] = {3, 7, 2, 9, 1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    bubble(arr, size);
+    for(int i = 0; i<size; i++){
+        std::cout << arr[i] << " ";
+    }
+}
