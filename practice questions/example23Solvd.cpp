be13c3e145996ca7ccc2d7ca9c591bd7b93859dc
Anurag kumar <anuragkumar1252003@gmail.com>
2026-08-24 00:06:23 +0530

    check if array is sorted and rotated

--- practice questions/example23Solvd.cpp
+++ practice questions/example23Solvd.cpp
@@ -23,19 +23,18 @@
 // You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
  
 #include<iostream>
-bool check(int arr[], int n){
+bool check(int arr[], int n){ 
     int count = 0;
     for (int i = 0; i < n; i++){
-        if(arr[i] < arr[i+1]){
+        if(arr[i] > arr[(i+1)%n]){
             count++;
         }
     }
-    return count <=1;
+    return count <= 1;
 }
 
 int main(){
-    int arr[] = {2,1,3,4};
+    int arr[] = {3,4,5,1,2};
     int size = sizeof(arr)/sizeof(arr[0]);
     std::cout<<check(arr, size);
-    return 0;
 }
