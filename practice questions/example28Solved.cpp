584462acc68ec79c484e9501b3a49521545ec601
Anurag kumar <anuragkumar1252003@gmail.com>
2026-09-02 00:10:34 +0530

    add initial implementation for checking permutation of strings S1 and S2

---
+++ practice questions/example28Solved.cpp
@@ -0,0 +1,16 @@
+//given two strings S1 and S2, return true if S2 contains a permutation(A permutation is a rearrangement of all the characters of a string.) of S1 , or false otherwise.
+//In other words, return true if one of S1's permutations is the substring of S2.
+
+
+// Example 1:
+// Input: s1 = "ab", s2 = "eidbaooo"
+// Output: true
+// Explanation: s2 contains one permutation of s1 ("ba").
+
+// Example 2:
+// Input: s1 = "ab", s2 = "eidboaoo"
+// Output: false
+
+// Constraints:
+// 1 <= s1.length, s2.length <= 10^4
+// s1 and s2 consist of lowercase English letters.
