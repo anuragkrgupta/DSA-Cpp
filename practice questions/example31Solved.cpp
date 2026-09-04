13e19695e4315aae367b381e72e182fdba70cfae
Anurag kumar <anuragkumar1252003@gmail.com>
2026-09-04 23:16:12 +0530

    add implementation for GCD and LCM calculation using Euclid's algo

---
+++ practice questions/example31Solved.cpp
@@ -0,0 +1,19 @@
+// GCD/HCF
+#include <iostream>
+using namespace std;
+
+int GCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return GCD(b, a % b);
+}
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int gcd = GCD(a, b);
+    int lcm = (a * b)/gcd;
+    cout << "GCD = " << gcd << endl;
+    cout << "LCM = " << lcm << endl;
+    return 0;
+}
