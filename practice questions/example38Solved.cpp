cb15df77404c95106c787c1ca843f6a1100ecc77
Anurag kumar <anuragkumar1252003@gmail.com>
2026-09-13 22:54:27 +0530

    brute force and optimal approaches for substring calculation

--- practice questions/example38Solved.cpp
+++ practice questions/example38Solved.cpp
@@ -7,6 +7,8 @@
 
 // aim for O(n) times -- not O(n^2) or O(n^3);
 
+
+//brute force approach
 int sub_string(string s){
     int ans = 0;
     for(int i = 0; i<s.length(); i++){
@@ -20,4 +22,24 @@ int sub_string(string s){
             ans = max(and, j-i+1);
         }
     }
+}
+
+//optimal approach for sliding window + hashing 
+int sub_string(string s){
+    vector<int> freq(256, 0);
+
+    int left = 0;
+     int ans = 0;
+     for(int right = 0; right< s.length(); right++){
+        freq[s[right]]  ++;
+        while(freq[s[right]] > 1){
+            freq[s[left]] --;
+            left ++;
+
+        }
+        ans = max(ans, right-left+1);
+
+     }
+     return ans;
+
 }
