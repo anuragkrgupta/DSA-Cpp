cf58340f44f4b14d6d91658b91ca2d831f7638bb
Anurag kumar <anuragkumar1252003@gmail.com>
2026-09-13 23:13:54 +0530

    optimize longest substring calculation using sliding window and hashing

--- practice questions/example38Solved.cpp
+++ practice questions/example38Solved.cpp
@@ -24,22 +24,22 @@ int sub_string(string s){
     }
 }
 
-//optimal approach for sliding window + hashing 
-int sub_string(string s){
-    vector<int> freq(256, 0);
+//optimal approach from sliding window + hashing 
 
-    int left = 0;
-     int ans = 0;
-     for(int right = 0; right< s.length(); right++){
-        freq[s[right]]  ++;
-        while(freq[s[right]] > 1){
-            freq[s[left]] --;
-            left ++;
-
+class Solution {
+public:
+    int sub_string(string s) {
+        unordered_map<char, int> freq;
+        int left = 0;
+        int ans = 0;
+        for(int right = 0; right < s.length(); right++) {
+            while(freq[s[right]] > 0) {
+                freq[s[left]]--;
+                left++;
+            }
+            freq[s[right]]++;
+            ans = max(ans, right - left + 1);
         }
-        ans = max(ans, right-left+1);
-
-     }
-     return ans;
-
-}
+        return ans;
+    }
+};
