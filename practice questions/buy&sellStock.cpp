0a9057a51170c9facc76057ec7945df4a083edff
Anurag kumar <anuragkumar1252003@gmail.com>
2026-09-17 23:30:51 +0530

    fix variable name and update comments for maxProfit function

--- practice questions/buy&sellStock.cpp
+++ practice questions/buy&sellStock.cpp
@@ -32,7 +32,7 @@
 
 int maxProfit(vector<int> &prices)
 {
-    int minp = INT_MAX;
+    int minP = INT_MAX;
     int maxP = 0;
     for (int i = 0; i < prices.size(); i++)
     {
@@ -49,3 +49,29 @@ int maxProfit(vector<int> &prices)
     }
     return maxP;
 }
+
+
+// You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+// On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
+
+// Find and return the maximum profit you can achieve.
+
+// Example 1:
+// Input: prices = [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+// Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+// Total profit is 4 + 3 = 7.
+
+
+// Example 2:
+// Input: prices = [1,2,3,4,5]
+// Output: 4
+// Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+// Total profit is 4.
+
+// Constraints:
+// 1 <= prices.length <= 3 * 104
+// 0 <= prices[i] <= 104
+
